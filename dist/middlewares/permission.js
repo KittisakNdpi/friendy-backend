3569db6ec493eaf27368a1b7403dd890b0815c3a
KittisakNdpi <kittisak.ndpi@outlook.com>
2023-03-27 02:11:36 +0700

    update remove folder dist

--- dist/middlewares/permission.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = (allowed) => {
-    return (req, res, next) => {
-        console.log(req.headers);
-        next();
-    };
-};
